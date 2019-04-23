d64cc9fba67879f29b424f4ce56c05509e680981
JRSgit <34550441+JRSgit@users.noreply.github.com>
2019-04-23 07:25:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use('/admin',(req,res) =>{
 });
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(require('./routes'));
 
